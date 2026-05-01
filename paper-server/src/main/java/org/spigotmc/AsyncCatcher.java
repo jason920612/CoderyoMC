b11d34a151a59b61ef9cf0a1a61fc40038bf47b1
jason920612 <jasonya2206@gmail.com>
2026-05-01 18:20:10 +0800

    Wire async API checks to Coderyo scheduler safety

--- paper-server/src/main/java/org/spigotmc/AsyncCatcher.java
+++ paper-server/src/main/java/org/spigotmc/AsyncCatcher.java
@@ -1,12 +1,16 @@
 package org.spigotmc;
 
+import io.papermc.paper.configuration.GlobalConfiguration;
 import net.minecraft.server.MinecraftServer;
 
 public class AsyncCatcher {
 
     public static void catchOp(String reason) {
         if (!ca.spottedleaf.moonrise.common.util.TickThread.isTickThread()) { // Paper - chunk system
-            MinecraftServer.LOGGER.error("Thread {} failed main thread check: {}", Thread.currentThread().getName(), reason, new Throwable()); // Paper
+            final GlobalConfiguration configuration = GlobalConfiguration.get();
+            if (configuration == null || configuration.coderyo == null || configuration.coderyo.schedulerSafety == null || configuration.coderyo.schedulerSafety.logAsyncApiViolations) {
+                MinecraftServer.LOGGER.error("Thread {} failed main thread check: {}", Thread.currentThread().getName(), reason, new Throwable()); // Paper
+            }
             throw new IllegalStateException("Asynchronous " + reason + "!");
         }
     }
